aee3d6dccbbff4efc8c04b0da20db967c74bae4f
DoneKan <kgzduncan@gmail.com>
2024-09-10 21:15:36 +0300

    added requested changes to team,index and about pages

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 (function ($) {
     "use strict";
-    
+
     // Initiate the wowjs
     new WOW().init();
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
@@ -14,11 +14,11 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
-    
-    
+
+
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 90) {
@@ -29,8 +29,8 @@
             $('.carousel, .page-header').css("margin-top", "0");
         }
     });
-    
-    
+
+
     // Dropdown on mouse hover
     $(document).ready(function () {
         function toggleNavbarMethod() {
@@ -47,31 +47,34 @@
         toggleNavbarMethod();
         $(window).resize(toggleNavbarMethod);
     });
-    
-    
+
+
     // jQuery counterUp
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
         time: 2000
     });
-    
-    
+
+
     // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
+    document.addEventListener('DOMContentLoaded', function () {
+        var videoModal = document.getElementById('videoModal');
+        var videoPlayer = document.getElementById('videoPlayer');
+
+        videoModal.addEventListener('show.bs.modal', function (event) {
+            var button = event.relatedTarget;
+            var videoSrc = button.getAttribute('data-src');
+            videoPlayer.querySelector('source').setAttribute('src', videoSrc);
+            videoPlayer.load();
         });
-        console.log($videoSrc);
 
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
+        videoModal.addEventListener('hidden.bs.modal', function () {
+            videoPlayer.pause();
+            videoPlayer.querySelector('source').removeAttribute('src');
+            videoPlayer.load();
+        });
     });
+
 
 
     // Testimonial Slider
@@ -93,33 +96,33 @@
         slidesToShow: 3,
         asNavFor: '.testimonial-slider'
     });
-    $('.testimonial .slider-nav').css({"position": "relative", "height": "160px"});
-    
-    
+    $('.testimonial .slider-nav').css({ "position": "relative", "height": "160px" });
+
+
     // Blogs carousel
     $(".related-slider").owlCarousel({
         autoplay: true,
         dots: false,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="fa fa-angle-left" aria-hidden="true"></i>',
             '<i class="fa fa-angle-right" aria-hidden="true"></i>'
         ],
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            576:{
-                items:1
+            576: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             }
         }
     });
-    
-    
+
+
     // Portfolio isotope and filter
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
@@ -130,8 +133,8 @@
         $("#portfolio-flters li").removeClass('filter-active');
         $(this).addClass('filter-active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        portfolioIsotope.isotope({ filter: $(this).data('filter') });
     });
-    
+
 })(jQuery);
 
